8811a245f6a44c5d45d18dd981e6fee04d4c1843
yuqi-wang-forever <1915780288@qq.com>
2023-06-02 21:56:21 +0800

    基本的变量

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # kotlin-tutorial
 kotlin学习
+## 变量
+    var声明可变变量
+    val声明不可变常量
+``` kotlin
+ 指定变量类型
+ var name: String = ""
+ 
+```
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main(){
+    var name: String = "yuqi"
+    println("Hello,${name}")
+
+    name = "Jack"
+    println("Hello,$name")
+
+    val age: Int = 24
+
+    println("your age is $age")
 
 }
